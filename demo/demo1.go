97a6a06d6a03f9f0db96cf2ccfca0cff3cb6c896
pastebt <maye1968@gmail.com>
2015-05-15 21:24:18 -0700

    update demo

--- demo/demo1.go
+++ demo/demo1.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-//    "os"
     "fmt"
     "coap"
 )
@@ -24,11 +23,7 @@ type myArg struct {
 func main() {
     m := myArg{Passwd: "123456"}
     args := coap.Parse(&m)
-    //fmt.Println("uname =", m.Uname)
-    //fmt.Println(os.Args)
-    //fmt.Fprint(os.Stderr, "err msg\n")
     fmt.Printf("-n = %s, -p = '%s'\n", m.Uname, m.Passwd)
     fmt.Printf("act = %s\n", m.Act)
     fmt.Printf("args = %v\n", args)
-    //os.Exit(1)
 }
